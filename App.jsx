6fc5cccbec1eb3c72d02ff3295ebd99fe30d554f
nicolaskribas <nicolaskribas@gmail.com>
2019-10-10 15:52:58 -0300

    initial commit on sign up screen

--- App.jsx
+++ App.jsx
@@ -1,19 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  View,
+  Button
+} from "native-base";
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Hello World!</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center"
-  }
-});
+const App = () => {
+  return (
+    <Container>
+      <Content>
+        <Text>Informações pessoais</Text>
+        <Form>
+          <Item floatingLabel>
+            <Label>Nome Completo</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>E-mail</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>Senha</Label>
+            <Input />
+          </Item>
+          <Item floatingLabel>
+            <Label>Confirmar Senha</Label>
+            <Input />
+          </Item>
+        </Form>
+        <View>
+          <Button>
+            <Text>Cancelar</Text>
+          </Button>
+          <Button>
+            <Text>Próximo</Text>
+          </Button>
+        </View>
+      </Content>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/screens/SignUpScren.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SignUpScren = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default SignUpScren;
