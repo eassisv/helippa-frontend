86f72cfb86bf3236be3a52cc70b9374775a14126
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2019-10-29 17:15:04 -0300

    Iniciando Home

--- App.jsx
+++ App.jsx
@@ -1,13 +1,81 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { View, Text, FlatList, StyleSheet, ScrollView } from "react-native";
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Hello World!</Text>
+state = {
+  data: [
+    { id: 0, full_name: "Repo 1" },
+    { id: 1, full_name: "Repo 2" },
+    { id: 2, full_name: "Repo 3" },
+    { id: 3, full_name: "Repo 4" },
+    { id: 4, full_name: "Repo 5" }
+  ]
+};
+
+renderItem = ({ item }) => (
+  <View style={styles.listItem}>
+    <Text>{item.full_name}</Text>
   </View>
 );
 
+const App = () => (
+  <ScrollView>
+    <View>
+      <FlatList
+        horizontal
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+    <View>
+      <FlatList
+        horizontal
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+    <View>
+      <FlatList
+        horizontal
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+    <View>
+      <FlatList
+        horizontal
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+    <View>
+      <FlatList
+        horizontal
+        style={{ marginTop: 30 }}
+        contentContainerStyle={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  </ScrollView>
+);
+
 const styles = StyleSheet.create({
+  list: {
+    paddingHorizontal: 20
+  },
   container: {
     alignItems: "center",
     backgroundColor: "#fff",
