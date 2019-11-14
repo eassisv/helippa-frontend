3d7bc19966e48f8b6193a34101b9d3533b1b838a
Andrew M. Silva <andrewsaxx@gmail.com>
2019-11-14 15:02:23 -0300

    Starting development of login screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,23 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Alert, View, Image, TextInput, Button } from "react-native";
+
 import ModalLogo from "../components/ModalLogo";
-import styles from "../styles";
+import Styles from "../styles";
+
+const logo = require("../../assets/logo-e-escrita-transparente-vertical.png");
 
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showModal: true };
+    this.state = {
+      showModal: true,
+      email: "",
+      password: ""
+    };
+  }
+
+  onLogin() {
+    const { username, password } = this.state;
+
+    Alert.alert("Credentials", `${username} + ${password}`);
   }
 
   render() {
     const { showModal } = this.state;
     return (
-      <View style={styles.container}>
+      <View style={Styles.darkcyanContainer}>
         <ModalLogo
           visible={showModal}
           onDismiss={() => this.setState({ showModal: false })}
         />
-        <Text>Hello</Text>
+        <Image source={logo} style={Styles.logo} />
+        <View style={{ flex: 3 }}>
+          <TextInput
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            placeholder="Email"
+            style={Styles.input}
+          />
+          <TextInput
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            placeholder="Password"
+            secureTextEntry
+            style={Styles.input}
+          />
+          <Button
+            title="Login"
+            style={Styles.input}
+            onPress={this.onLogin.bind(this)}
+          />
+        </View>
       </View>
     );
   }
