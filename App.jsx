34a3d06b685576ad31a6985896950b2fc806815d
aluno-uffs <aluno-uffs@lab-cc.cco.uffs.edu.br>
2019-11-14 16:10:41 -0300

    botão

--- App.jsx
+++ App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { Container, Content, Button, Text } from "native-base";
 
 const App = () => (
-  <View style={styles.container}>
-    <Image
-      source={{
-        uri:
-          "https://images.unsplash.com/photo-1520390138845-fd2d229dd553?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80"
-      }}
-      style={{ height: 250, width: 500, opacity: 0.5 }}
-    />
-    <Text>Hello World!</Text>
-  </View>
+  <Container style={styles.container}>
+    <Content>
+      <Image
+        source={{
+          uri:
+            "https://images.unsplash.com/photo-1520390138845-fd2d229dd553?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80"
+        }}
+        style={{ height: 250, width: 500, opacity: 0.5 }}
+      />
+      <Button style={styles.button}>
+        <Text style={styles.textButton}> Tenho Interesse </Text>
+      </Button>
+    </Content>
+  </Container>
 );
 
 const styles = StyleSheet.create({
@@ -20,6 +25,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#3C4859",
     flex: 1,
     justifyContent: "flex-start"
+  },
+
+  button: {
+    borderRadius: 10,
+    backgroundColor: "#fffacd"
+  },
+
+  textButton: {
+    color: "#008b8b"
   }
 });
 
