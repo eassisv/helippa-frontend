be7e134c93db883182acb398f16d43fc503688f1
Éverton de Assis <eassis.vieira@gmail.com>
2019-11-21 14:45:08 -0300

    updated project structure to create components

---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ExampleComponent = () => {
+  return (
+    <View>
+      <Text>Example Component</Text>
+    </View>
+  );
+};
+
+export default ExampleComponent;
