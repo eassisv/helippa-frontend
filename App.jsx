3afdd9f0625b43cbf31fa45651fea0709a82ae77
aluno-uffs <aluno-uffs@lab-cc.cco.uffs.edu.br>
2019-12-03 17:19:51 -0300

    sla

--- App.jsx
+++ App.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { View } from "react-native";
 import Select from "./src/components/Select";
 
-const App = () => (
-  <View
-    style={{
-      backgroundColor: "red",
-      flex: 1,
-      alingItems: "center",
-      justifyContent: "center"
-    }}
-  >
-    <Select />
-  </View>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selecionado: 2
+    };
+  }
 
-export default App;
+  funct = selecionado => {
+    this.setState({ selecionado });
+  };
+
+  render() {
+    const { selecionado } = this.state;
+    return (
+      <View
+        style={{
+          backgroundColor: "red",
+          flex: 1,
+          alingItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Select
+          onChange={value => this.funct(value)}
+          selectedValue={selecionado}
+          items={[
+            { value: 1, label: "oi" },
+            { value: 2, label: "oi2" }
+          ]}
+        />
+      </View>
+    );
+  }
+}
