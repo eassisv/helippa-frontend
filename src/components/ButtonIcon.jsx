5a375c8ed36984f9e185f2241e5f9f012ff13b26
Everton Assis <eassis.vieira@gmail.com>
2019-12-04 21:44:46 -0300

    updated prop names and styles

--- src/components/ButtonIcon.jsx
+++ src/components/ButtonIcon.jsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-const ButtonIcon = ({ title, writeColor, sizeIcon, sizeFont, fundColor, nameIcon }) 
-=> (
-  <TouchableOpacity
-    style={{
-      backgroundColor: fundColor,
+const ButtonIcon = ({
+  title,
+  color,
+  iconSize,
+  fontSize,
+  backgroundColor,
+  iconName
+}) => {
+  const styles = createStyles(color, fontSize, backgroundColor);
+
+  return (
+    <TouchableOpacity style={styles.button}>
+      <FontAwesome name={iconName} style={styles.icon} />
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const createStyles = (color, fontSize, backgroundColor) =>
+  StyleSheet.create({
+    button: {
+      backgroundColor,
       flexDirection: "row",
       borderRadius: 5
-    }}
-  >
-    <Icon
-      name={nameIcon}
-      style={{ color: writeColor, fontSize: sizeIcon, padding: 5 }}
-    />
-    <Text style={{ color: writeColor, fontSize: sizeFont, padding: 8 }}>
-      {title}
-    </Text>
-  </TouchableOpacity>
-);
+    },
+    title: { color, fontSize, padding: 8 },
+    icon: { color, padding: 5 }
+  });
 
 export default ButtonIcon;
