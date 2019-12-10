e84cbf4348ee33c044e2a38b3fde7d90340adda9
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2019-12-09 23:25:59 -0300

    added create event screen

---
+++ src/screens/CreateEventScreen.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import CustomTextInput from '../components/CustomTextInput';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import LoadingButton from '../components/LoadingButton';
+
+class CreateEventScreen extends React.Component {
+  constructor({navigation}) {
+    super({navigation});
+    this.state = {
+      showDatePicker: false,
+      date: new Date(),
+      name: '',
+      description: '',
+      mode: 'date',
+    };
+  }
+
+  dateString() {
+    return this.state.date.toGMTString();
+    // const day = this.state.date.getDate();
+    // const month = this.state.date.getMonth() + 1;
+    // const year = this.state.date.getFullYear();
+    // return `${day}/${month}/${year}`;
+  }
+
+  setDay(event, date) {
+    console.log('set date called with');
+    console.log(date);
+    this.setState(prevState => {
+      const newDate = new Date(prevState.date);
+      newDate.setFullYear(date.getFullYear());
+      newDate.setMonth(date.getMonth());
+      newDate.setDate(date.getDate());
+      return {
+        date: newDate,
+        showDatePicker: false,
+      };
+    });
+  }
+
+  setTime(event, date) {
+    console.log('set time ');
+    this.setState(prevState => {
+      const newDate = new Date(prevState.date);
+      newDate.setHours(date.getHours(), date.getMinutes());
+      return {
+        date: newDate,
+      };
+    });
+  }
+
+  onChangeName(name) {
+    this.setState(() => ({
+      name: name,
+    }));
+  }
+
+  onChangeDesc(desc) {
+    this.setState(() => ({
+      description: desc,
+    }));
+  }
+
+  showDayPicker() {
+    this.setState(() => ({
+      showDatePicker: true,
+      mode: 'date',
+    }));
+  }
+
+  showTimePicker() {
+    this.setState(() => ({
+      showDatePicker: true,
+      mode: 'time',
+    }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Crie um Evento</Text>
+        <CustomTextInput
+          name="name"
+          placeholder="Nome do evento"
+          style={styles.input}
+          onChangeText={name => this.onChangeName(name)}
+          value={this.state.name}
+        />
+        <CustomTextInput
+          name=""
+          placeholder="Descrição do Evento"
+          style={styles.input}
+          onChangeText={desc => this.onChangeDesc(desc)}
+          value={this.state.description}
+        />
+        <LoadingButton
+          style={styles.button}
+          onPress={() => this.showDayPicker()}
+        >
+          Alterar data
+        </LoadingButton>
+        <LoadingButton
+          style={styles.button}
+          onPress={() => this.showTimePicker()}
+        >
+          Alterar horário
+        </LoadingButton>
+        {this.state.showDatePicker && (
+          <DateTimePicker
+            value={this.state.date}
+            onChange={this.setDay}
+            is24Hour={true}
+            mode={this.state.mode}
+          />
+        )}
+        <Text>
+          Data do Evento: {this.dateString()}, -{this.state.description}-
+        </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#444',
+  },
+  input: {
+    marginTop: 20,
+    elevation: 3,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'darkcyan',
+  },
+});
+
+export default CreateEventScreen;
